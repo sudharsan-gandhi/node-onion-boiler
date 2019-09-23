15b20bcc569e5e5eada80dc83bc738b33fb6a86f
sudharsan gandhi <sudharsan.gandhi@cesltd.com>
2019-09-23 19:49:43 +0530

    Added base resolver initial setup

--- src/graphql/user/user.resolver.ts
+++ src/graphql/user/user.resolver.ts
@@ -1,23 +1,30 @@
 import { Resolver, Mutation, Args, Query } from 'type-graphql';
 import { User } from '../../entity/user';
-import { Conflict } from '../../utils/exceptions';
 import { UserInput } from './user.input';
+import { createBaseResolver } from '../base.resolver';
 
-@Resolver(User)
-export class UserResolver {
-    @Query(() => [User])
-    public async getAll(): Promise<User[]> {
-        return await User.find({});
-    }
+const userBaseResolver = createBaseResolver('User', User, UserInput);
 
-    @Mutation(() => User)
-    public async save(@Args() { name }: UserInput): Promise<User> {
-        const created = await User.create({name}).save();
-        console.log('created', created);
-        if (!!created) {
-            return created;
-        } else {
-            throw new Conflict('Cant create new user');
-        }
-    }
+@Resolver( () => User)
+export class UserResolver extends userBaseResolver {
+    // write additional methods in here for User
 }
+
+// @Resolver(User)
+// export class UserResolver {
+//     @Query(() => [User])
+//     public async getAll(): Promise<User[]> {
+//         return await User.find({});
+//     }
+
+//     @Mutation(() => User)
+//     public async save(@Args() { name }: UserInput): Promise<User> {
+//         const created = await User.create({name}).save();
+//         console.log('created', created);
+//         if (!!created) {
+//             return created;
+//         } else {
+//             throw new Conflict('Cant create new user');
+//         }
+//     }
+// }
